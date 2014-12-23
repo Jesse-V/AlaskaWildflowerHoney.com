8f1afdd94a165490c3f9b6ce775ab3b561278aad
Jesse Victors <jvictors@jessevictors.com>
2014-12-22 23:05:04 -0900

    organized breed writeups into list form

--- order_bees.php
+++ order_bees.php
@@ -60,25 +60,55 @@
             Which breed of honeybees is right for me?
         </div>
         <div id="breedWriteup">
-            <p>
-                If you are new, that's not a problem. I've provided a short discussion below on bee types that I hope will help you choose the type of queen that will work best for you.
+            <p class="intro">
+                If you are new, that's not a problem. I've provided a short outline below on bee types that I hope will help you choose between two common breeds of honeybees. While the characteristics that are generally how the traits of the breed go, not every queen may get the message and the hive may be a bit differently than what is outlined here.
             </p>
-            <p>
-                While the characteristics that are laid out here are generally how the traits of the queens go, beekeepers will tell you that each queen may not read the instruction manual and she may do a bit differently than what is outlined here. Since the two types of queens that are most readily available are the Italian and the Carniolan, those are the ones I will write about.
-            </p>
-            <p>
-                I think that it is easier to understand the traits if you look at the root stock of where the bee comes from. Italian queens tend to brood well, no matter what. The large work force brings in a good crop during a nectar flow. For this reason, Italian bees are perhaps the most popular bee in commercial and hobby beekeeping.  They come from a Mediterranean climate and like big families. The preference of a larger family means that they have a reduced tendency to swarm, but a bigger brood nest also means that they will also eat everything in sight and then go out to rob the surrounding countryside (including the neighboring hives) when there is a lack of nectar coming in. The propensity to rob is perhaps their biggest fault because it can make the bees defensive if this type of activity is allowed to go on in the bee yard. The Italian bees will also brood right through a nectar dearth expecting that there will be more flowers blooming soon.  This means that they are not at all careful with their food and can even starve out because they fed all of the reserves to the developing larvae.
-            </p>
-            <p>
-                <b>If your management style has you skipping hive checks in mid-summer, or are drawing out foundation into comb, are predicting a warm summer, or have no intention on wintering your bees then Italian bees is the one for you.</b>
-            </p>
-            <p>
-                Carniolan bees, in contrast to the Italians, come from the mountains of Europe away from that nice Mediterranean climate. They are used to a short spring so they have to brood up quickly. Summer there is also shorter than the one granted to their Italian cousins. Therefore, if they want to make new colonies they have to do it soon; so the tendency to raise a new queen and swarm is much more pronounced than the Italian. They will forage in cooler weather. However, when the nectar flow stops they don't know if the season is over or if it is just a dry spell, so they prefer the safer move of shutting the queen down and stop raising brood to conserve food. This tendency helps make them a good choice for wintering but requires extra management in the summer to increase brood during nectar shortage. Perhaps it is not in the best interest of the bees in the mountains to start a war over food during the short summer because it damages the health of all the hives. Whatever the case, the Carniolan bee has a low tendency to rob other hives and equipment.
-            </p>
-            <p>
-                <b>If your management style consists of regular hive checks for swarming, perhaps a bit of food during the build-up period, if you keep bees in a cooler climate, if you are thinking about wintering, or if you don't want to worry about covering supers and frames during summer hive checks to prevent robbing, the Carniolan breed might be the choice for you.</b>
-            </p>
-            <p>
+            <div class="breedDesc">
+                <p class="breedIntro">
+                    Italian bees come from a Mediterranean climate and like big families.
+                </p>
+                <ul>
+                    <li><b>They tend to brood well, no matter what.</b> The large work force brings in a good crop during a nectar flow. For this reason, Italian bees are perhaps the most popular bee in commercial and hobby beekeeping.</li>
+                    <li><b>They have a reduced tendency to swarm.</b></li>
+                    <li><b>They have large food consumption</b> and will eat everything in sight and then go out to rob the surrounding countryside (including the neighboring hives) when there is a lack of nectar coming in. This is perhaps their biggest fault because it can make them defensive in the bee yard.</li>
+                    <li><b>They will also brood right through a nectar famine</b>, expecting that there will be more flowers blooming soon. This means that they are not careful with their food and can even starve out because they fed all of the reserves to the developing larvae.
+                </ul>
+                <div class="conclusion">
+                    Therefore,
+                    <ol>
+                        <li>If your management style has you skipping hive checks in mid-summer, or</li>
+                        <li>If you are are drawing out foundation into comb, or</li>
+                        <li>If are predicting a warm summer.</li>
+
+                    </ol>
+                    then Italian bees might be the ones for you.
+                </div>
+            </div>
+            <div class="breedDesc">
+                <p class="breedIntro">
+                    Carniolan bees, in contrast to the Italians, come from the mountains of Europe.
+                </p>
+                <ul>
+                    <li><b>They are used to a short spring</b> so they want to brood up quickly. Summer there is also shorter than the one granted to their Italian cousins.</li>
+                    <li><b>They have a pronounced tendency to raise a new queen and swarm.</b></li>
+                    <li><b>They will forage in cooler weather.</b></li>
+                    <li><b>During nectar feminines, they tend to shut the queen down and stop raising brood to conserve food.</b> They don't know if the season is over or if it is just a dry spell, so this is the safer move.</li>
+                    <li><b>They are a good choice for wintering.</b></li>
+                    <li><b>They require extra management and feeding in the summer</b> to increase brood during nectar shortage.</li>
+                    <li><b>They have a low tendency to rob</b> other hives and equipment.</li>
+                </ul>
+                <div class="conclusion">
+                    Therefore,
+                    <ol>
+                        <li>If your management style consists of regular hive checks for swarming, perhaps giving a bit of food during the build-up period, or</li>
+                        <li>If you keep bees in a cooler climate, or</li>
+                        <li>If you are thinking about wintering, or</li>
+                        <li>If you don't want to worry about covering supers and frames to prevent robbing,</li>
+                    </ol>
+                    then the Carniolan breed might be the choice for you.
+                </div>
+            </div>
+            <p class="intro">
                 The Italian bee is popular in the Interior (Fairbanks) area. The Carniolan is popular on the Kenai Peninsula. I run about half and half here at Big Lake.
             </p>
         </div>
