ed3cd8ea63dfa9ef49cccfe11da8397e573a4235
Jesse Victors <jvictors@jessevictors.com>
2015-01-01 14:55:41 -0900

    sanitized checkout GET for #41

--- checkout/5card_receipt.php
+++ checkout/5card_receipt.php
@@ -8,18 +8,25 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'/assets/common/header.php'); //opening HTML
 
 
+    //XSS sanitize, https://stackoverflow.com/questions/1996122/how-to-prevent-xss-with-html-php
+    $input = array();
+    foreach ($_GET as $key => $value) {
+        $input[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+
+
     echo '<h1>Your receipt</h1>';
 
-    if (empty($_GET) || !isset($_GET['rc']) || !isset($_GET['hash']) || empty($_SESSION))
+    if (empty($input) || !isset($input['rc']) || !isset($input['hash']) || empty($_SESSION))
     { //check to see if we received the expected information
         echo "
             <p>
                 Oops! We cannot show you a receipt because not enough information was sent to this page. One possible explanation is that you have already completed the transaction. If you feel that you have reached this message in error, please notify us by sending an email to <a href=\"mailto:jvictors@jessevictors.com?Subject=Receipt%20error\" target=\"_top\">jvictors@jessevictors.com</a> and we will try to address this issue promptly. We apologize for the inconvenience.
             </p>";
     }
-    else if ($_GET['rc'] == 1 || strpos($_GET['resp'], "TESTMODE") !== false)
+    else if ($input['rc'] == 1 || strpos($input['resp'], "TESTMODE") !== false)
     {
-        if ($_GET['hash'] == hash("sha256", $md5_setting.$_GET['rc'].$_GET['id'].$md5_setting))
+        if ($input['hash'] == hash("sha256", $md5_setting.$input['rc'].$input['id'].$md5_setting))
         { //check signature from Authorize.net and from us
 
             $firstName = $_SESSION['paymentInfo']['x_first_name'];
@@ -27,7 +34,7 @@
 
             echo "
                 <p>
-                    Thank you ".$firstName."! Your payment (transaction ".$_GET['id'].") was successful. Your order has been sent to us and we will process it shortly. Please check your email for receipts.
+                    Thank you ".$firstName."! Your payment (transaction ".$input['id'].") was successful. Your order has been sent to us and we will process it shortly. Please check your email for receipts.
                 </p>
                 <p>
                    Thanks for ordering online!
@@ -38,7 +45,7 @@
             sendCardDadEmail2($_SESSION['paymentInfo'],
                 'AlaskaWildflowerHoney.com <DoNotReply@stevesbees.com>',
                 "Online Order Complete for ".$firstName.' '.$lastName,
-                $firstName, $lastName, $_GET['id']);
+                $firstName, $lastName, $input['id']);
 
             unset($_SESSION);
             session_destroy();
@@ -54,7 +61,7 @@
     {
         echo "
             <p>
-                Oops! Something went wrong during the transaction. Authorize.net was unable to fully process your card for the following reason: \"".htmlentities($_GET['resp'])."\" The card's number may have been mistyped or has expired, the address or ZIP code may not match, or something else is wrong. Please try again or use a different card. You have also received an email about this.
+                Oops! Something went wrong during the transaction. Authorize.net was unable to fully process your card for the following reason: \"".htmlentities($input['resp'])."\" The card's number may have been mistyped or has expired, the address or ZIP code may not match, or something else is wrong. Please try again or use a different card. You have also received an email about this.
             </p>";
 
         $firstName = $_SESSION['paymentInfo']['x_first_name'];
@@ -63,12 +70,12 @@
         sendFailedCustomerEmail($_SESSION['paymentInfo'],
             'Alaska Wildflower Honey <victors@mtaonline.net>',
             "The transaction has failed",
-            $firstName, htmlentities($_GET['resp']));
+            $firstName, htmlentities($input['resp']));
 
         sendFailedDadEmail($_SESSION['paymentInfo'],
             'AlaskaWildflowerHoney.com <DoNotReply@stevesbees.com>',
             $firstName.' '.$lastName."'s card transaction failed",
-            $firstName, $lastName, htmlentities($_GET['resp']));
+            $firstName, $lastName, htmlentities($input['resp']));
 
         echo '
             <form method="get" action="1cart_checkout.php">
