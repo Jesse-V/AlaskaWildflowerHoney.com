b93b6a8ec078858e847c13ff42ddb31634088f8a
Jesse Victors <jvictors@jessevictors.com>
2015-09-23 10:55:46 -0800

    Write website text in CommonMark

---
+++ text/education/May/index.php
@@ -0,0 +1,17 @@
+<?php //opening HTML
+    $_TITLE_ = "May - Education";
+    $_STYLESHEETS_ = array('/assets/css/education_documents.css');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/assets/common/header.php');
+    $_SESSION['customer'] = true;
+
+    require_once($_SERVER['DOCUMENT_ROOT']."/assets/CommonMark.php");
+    use League\CommonMark\CommonMarkConverter;
+    $converter = new CommonMarkConverter();
+
+    $filename = "home.txt";
+    $contents = fread(fopen($filename, "r"), filesize($filename));
+    echo $converter->convertToHtml($contents);
+
+    $_JS_ = array();
+    require_once($_SERVER['DOCUMENT_ROOT'].'/assets/common/footer.php'); //closing HTML
+?>
